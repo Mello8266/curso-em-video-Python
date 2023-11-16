9e10058f106967317ddf39c39be4d8b0e6c563fb
Vinnycius Melo <vinnycius.melo@gmail.com>
2023-11-16 15:23:52 -0300

    update

--- exercícios/ex037.py
+++ exercícios/ex037.py
@@ -1,14 +1,18 @@
 print('Conversão númerica')
 num = int(input('Isira um número inteiro: '))
-escolha = str(input('Escolha uma dessas conversões: binário, octal ou hexadecimal: '))
+escolha = str(input('Escolha uma dessas conversões: binário, octal ou hexadecimal: ')).lower()
 
-if escolha.lower() == 'binario' or 'binário':
+if escolha == 'binario' or 'binário':
     str = bin(num)
     print(f'O número {num} em binário vale {str[2:]}')
 
-elif escolha.lower() == 'octal':
+elif escolha == 'octal':
     str = oct(num)
     print(f'O número {num} em octal vale {str[2:]}')
+
+elif escolha == 'hexadecimal':
+    string = hex(num)
+    print(f'O número {num} em hexadecimal vale {string[2:]}')
 
 else:
     print('EROO -- Insira uma opção válida!')
