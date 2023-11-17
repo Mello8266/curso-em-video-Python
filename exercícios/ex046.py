af0530fe19e64c155e4503d584416911e42bb2f7
Vinnycius Melo <vinnycius.melo@gmail.com>
2023-11-16 22:32:31 -0300

    exercícios aula 13

---
+++ exercícios/ex046.py
@@ -0,0 +1,6 @@
+from time import sleep
+
+for c in range (1, 11):
+    print(f'{c}!')
+    sleep(1)
+print('KABUM!')
---
+++ exercícios/ex047.py
@@ -0,0 +1,6 @@
+print('Todos os número pares de 1 a 50')
+print('-=-' * 15)
+for c in range(0, 51, 2):
+    print(f'{c}')
+
+print('-=-' * 15)
