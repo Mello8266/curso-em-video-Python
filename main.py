00d56dd20d818c5d4a80cbd68f9d8da08d70afbf
Vinnycius Melo <vinnycius.melo@gmail.com>
2023-12-11 17:23:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-a = float(input('Insira a primeira medida: '))
-b = float(input('Insira a segunda medida: '))
-c = float(input('Insira a terceira medida: '))
-
-if a + b > c and b + c > a and c + a > b:
-    print('Com essas medidas é possível formar um triângulo.')
-
-else:
-    print('Com essas medidas é impossível formar um triângulo.')
+import antigravity
